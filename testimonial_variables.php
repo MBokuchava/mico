4e36d87e338d55072922ae2071a4afba29dd4e87
MBokuchava <Madlenbokuchava@gmail.com>
2023-10-01 11:57:16 +0400

    Testimonials

---
+++ testimonial_variables.php
@@ -0,0 +1,24 @@
+<?php
+// Testimonial
+// შევქმენით მასივი Testimonial-ებისთვის:
+$carouselItems = [
+    [
+        'clientName' => 'Morijorch',
+        'clientInfo' => 'Default model text',
+        'testimonial' => "editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various",
+        'isActive' => true, // დავაყენოთ პირველი თესთიმონიალი, როგორც აქტიური
+    ],
+    [
+        'clientName' => 'Rochak',
+        'clientInfo' => 'Default model text',
+        'testimonial' => "Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+        'isActive' => false,
+    ],
+    [
+        'clientName' => 'Brad Johns',
+        'clientInfo' => 'Default model text',
+        'testimonial' => "Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy, editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various",
+        'isActive' => false,
+    ],
+];
+?>
