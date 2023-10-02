e3d8719283006337d68c089d9d8f317e9a314168
MBokuchava <Madlenbokuchava@gmail.com>
2023-10-02 12:29:09 +0400

    Slider

---
+++ slider.php
@@ -0,0 +1,26 @@
+<?php
+// Slider Section
+// შევქმენით მასივი Slider-ისთვის:
+
+$sliderlItems = [
+    [
+        'title' => 'Mico Hospital',
+        'description' => 'When looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to',
+        'image' => 'images/slider-img.jpg',
+        'link' => 'Contact Us',
+    ],
+    [
+        'title' => 'Mico Hospital',
+        'description' => 'When looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to',
+        'image' => 'images/slider-img.jpg',
+        'link' => 'Contact Us',
+    ],
+    [
+        'title' => 'Mico Hospital',
+        'description' => 'When looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to',
+        'image' => 'images/slider-img.jpg',
+        'link' => 'Contact Us',
+    ],
+];
+
+?>
